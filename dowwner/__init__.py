a1acf52e265852724e64af523372f787db0ed597
10sr <8slashes+git@gmail.com>
2013-02-28 21:52:39 +0900

    add setup.py

--- dowwner/__init__.py
+++ dowwner/__init__.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
+"""dowwner - Very simple wiki program using markdown
+
+Wiki program working without external http server.
+Uses markdown for markup.
+"""
+
 import os
 
 from dowwner.server import start
+
+__version__ = "0.0.1"
 
 def main(port=2505, rootdir=os.getcwd()):
     return start(port=port, rootdir=rootdir)
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+import dowwner
+
+setup(name = "dowwner",
+      version = dowwner.__version__,
+      description = "Very simple wiki program using markdown",
+      long_description = dowwner.__doc__,
+      author = "10sr",
+      author_email = "sr10@sourceforge.org",
+      url = "https://10sr.github.com/dowwner",
+      download_url = "https://github.com/10sr/archsh_py/archive/master.zip",
+      packages = ["dowwner", "dowwner/markdown"],
+      scripts = ["bin/dowwner"],
+      keywords = "wiki",
+      classifiers=['License :: Public Domain']
+      )
