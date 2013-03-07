bb28013f429a8ebfdf7840866a2d5b6881efb7d4
10sr <8slashes+git@gmail.com>
2013-03-07 16:18:56 +0900

    small fixes

--- dowwner/__init__.py
+++ dowwner/__init__.py
@@ -7,26 +7,30 @@ Uses markdown for markup.
 """
 
 import os
-from socket import gethostname
-
-from dowwner.server import start
 
 __version__ = "0.0.1"
 
 def main(port=2505, rootdir=os.getcwd(), daemon=None, cgi=False):
     def f():
+        from dowwner.server import start
         return start(port=port, rootdir=rootdir)
 
     rootdir = os.path.realpath(rootdir)
+
     if cgi:
         from dowwner.cgi import main
         return main(rootdir)
+
     elif daemon:
-        from dowwner.daemon import main
+        from socket import gethostname
+        hostname = gethostname()
         pidfile = os.path.join(rootdir,
-                               ".".join((".dowwner", gethostname(), "pid")))
+                               ".".join((".dowwner", hostname, "pid")))
         logfile = os.path.join(rootdir,
-                               ".".join((".dowwner", gethostname(), "log")))
+                               ".".join((".dowwner", hostname, "log")))
+
+        from dowwner.daemon import main
         return main(daemon, pidfile, logfile, f)
+
     else:
         return f()
