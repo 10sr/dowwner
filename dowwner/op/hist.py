2e4250c193bf2612da2ee6823916fc4b8402f73c
10sr <8slashes+git@gmail.com>
2013-03-23 23:53:03 +0900

    add footer for hist page

--- dowwner/op/hist.py
+++ dowwner/op/hist.py
@@ -5,12 +5,18 @@
 import dowwner.op
 
 class OP_GET(dowwner.op.OP):
+    histfooter = """<hr />
+<a href="{name}">{name}</a>"""
+
     def __init__(self, file, path_):
         dowwner.op.OP.__init__(self, file, path_)
+
         ls = file.lshist(path_)
         c = ("<h1>{path}</h1>\n".format(path=path_.path) +
              "".join("""<a href="{name}">{name}</a><br />\n""".format(name=i)
                      for i in ls))
 
-        self.body = "\n".join(("<body>", c, "</body>"))
+        f = self.histfooter.format(name=(path_.base or "./"))
+
+        self.body = "\n".join(("<body>", c, f, "</body>"))
         return
