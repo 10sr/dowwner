3a8a9bfc13eec852a84e74655a65bcc5c4759ad1
10sr <8slashes+git@gmail.com>
2013-03-31 01:54:28 +0900

    specify output_format for markdown

--- dowwner/markdown/__init__.py
+++ dowwner/markdown/__init__.py
@@ -10,7 +10,9 @@ from dowwner.markdown import dwlinks
 class Markdown(markdown.Markdown):
     def __init__(self):
         dwl = dwlinks.makeExtension()
-        return markdown.Markdown.__init__(self, extensions=[dwl])
+        return markdown.Markdown.__init__(self,
+                                          extensions=[dwl],
+                                          output_format="xhtml1")
 
 if __name__ == "__main__":
     pass
