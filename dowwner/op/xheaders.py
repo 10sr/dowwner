0ee04686be54606b5389de5ad3dc19c89a4e9118
10sr <8.slashes@gmail.com>
2018-12-06 14:25:34 +0900

    Clear project
    Old version is available in oldversion branch.

--- dowwner/op/xheaders.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import dowwner.op.edit
-
-
-class ContentGET(dowwner.op.edit.ContentGET):
-    pass
--- tools/testcgi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export SERVER_NAME=example.com
-export SCRIPT_NAME=./cgi.py
-export PATH_INFO=/
-export QUERY_STRING=
-export REQUEST_METHOD=GET
-
-$SCRIPT_NAME
