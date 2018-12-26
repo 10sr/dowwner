9612e814d71c014da1a00a799fbbd9838df1a30b
10sr <8.slashes@gmail.com>
2018-12-26 19:36:56 +0900

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,20 +2,27 @@
 import os
 import sys
 from pathlib import Path
-
-
-# TODO: How to check if going to run tests?
-if sys.argv[1] == "test":
-    os.environ["DOWWNER_ENV"] = "test"
-    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
-else:
-    os.environ.setdefault("DOWWNER_ENV", "local")
-    os.environ[
-        "DJANGO_SETTINGS_MODULE"
-    ] = f"dowwner.settings_{os.environ['DOWWNER_ENV']}"
-
-os.environ["DOWWNER_BASE_DIR"] = str(Path(__file__).resolve().parent)
+from typing import List
 
 from django.core.management import execute_from_command_line
 
-execute_from_command_line(sys.argv)
+
+def main(argv: List[str]) -> int:
+    # TODO: How to check if going to run tests?
+    if argv[1] == "test":
+        os.environ["DOWWNER_ENV"] = "test"
+        os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
+    else:
+        os.environ.setdefault("DOWWNER_ENV", "local")
+        os.environ[
+            "DJANGO_SETTINGS_MODULE"
+        ] = f"dowwner.settings_{os.environ['DOWWNER_ENV']}"
+
+    os.environ["DOWWNER_BASE_DIR"] = str(Path(__file__).resolve().parent)
+
+    execute_from_command_line(sys.argv)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
