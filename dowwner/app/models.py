54bcd2d90b71b7377f1a83cca05d0fc6d5c8821e
10sr <8.slashes@gmail.com>
2018-12-26 17:08:04 +0900

    CharField -> TextField

--- dowwner/app/models.py
+++ dowwner/app/models.py
@@ -7,6 +7,6 @@ class Page(models.Model):
     created_at = models.DateTimeField("date created")
     updated_at = models.DateTimeField("date updated")
     # content is better?
-    markdown = models.CharField(max_length=5000, default="")
+    markdown = models.TextField(max_length=5000, default="")
     path = models.CharField(max_length=200, unique=True)
     # TODO: Add relation to created user and updated user
