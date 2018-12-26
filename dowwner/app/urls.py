6b0c15aba2c7ca859bde64cd802ddd2f9f6f4a8b
10sr <8.slashes@gmail.com>
2018-12-26 19:30:58 +0900

    Add view page

--- dowwner/app/urls.py
+++ dowwner/app/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import include, url
-from dowwner.app import views
+from . import views
 
 # TODO: Is this correct?
 app_name = "dowwner"
-urlpatterns = [url(r"^$", views.index, name="index")]
+urlpatterns = [
+    url(r"^$", views.index, name="index"),
+    # TODO: Ths URL is temporal
+    # Use regexp to allow slashes in path_
+    url(r"v/(?P<path_>[_0-9a-zA-Z/]+)", views.v, name="v"),
+]
--- dowwner/app/views.py
+++ dowwner/app/views.py
@@ -9,6 +9,17 @@ from . import models
 
 def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse(
-        f"""index {dir(request)}
-    <a href="admin">admin</a>"""
+        f"""index
+        <a href="v/test/page">test/page</a>
+        <a href="admin">admin</a>
+        <p>{dir(request)}</p>
+        """
     )
+
+
+def v(request: HttpRequest, path_: str) -> HttpResponse:
+    try:
+        p = models.Page.objects.get(path=path_)
+    except models.Page.DoesNotExist as e:
+        return HttpResponse(f"Not found: {path_}")
+    return HttpResponse(f"Content: {p.markdown}")
