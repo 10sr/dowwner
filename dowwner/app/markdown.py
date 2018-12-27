f66c1df8cbe0a056e9e0840cc0badbee64c640c3
10sr <8.slashes@gmail.com>
2018-12-27 13:50:29 +0900

    Add markdown using Markdown library

--- dowwner/app/markdown.py
+++ dowwner/app/markdown.py
@@ -1,6 +1,27 @@
 # TODO: Which package to put this file?
 
+import re
+
+import markdown
+from markdown.extensions.wikilinks import WikiLinkExtension
+
 import markdown2
 
-def to_html(markdown: str) -> str:
-    return markdown2.markdown(markdown)
+# TODO: Any way to strip [[]] from display text?
+LINK_PATTERNS = [(re.compile(r"\[\[([\w0-9_ -/.]+)\]\]"), r"\1")]
+
+
+def to_html_markdown2(source: str) -> str:
+    return markdown2.markdown(
+        source, extras=["link-patterns"], link_patterns=LINK_PATTERNS
+    )
+
+
+def to_html_Markdown(source: str) -> str:
+    return markdown.markdown(
+        # Cannot have slashes inside of blackets
+        source, extensions=[WikiLinkExtension(base_url="", end_url="")]
+    )
+
+
+to_html = to_html_Markdown
