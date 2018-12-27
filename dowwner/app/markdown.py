173aac8f762600fb560d878f01a65cd2fdc61f10
10sr <8.slashes@gmail.com>
2018-12-27 14:15:32 +0900

    Add underscore to private methods

--- dowwner/app/markdown.py
+++ dowwner/app/markdown.py
@@ -8,17 +8,17 @@ from markdown.extensions.wikilinks import WikiLinkExtension
 import markdown2
 
 # TODO: Any way to strip [[]] from display text?
-LINK_PATTERNS = [(re.compile(r"\[\[([\w0-9_ -/.]+)\]\]"), r"\1")]
+_LINK_PATTERNS = [(re.compile(r"\[\[([\w0-9_ -/.]+)\]\]"), r"\1")]
 
 
-def to_html_markdown2(source: str) -> str:
+def _to_html_markdown2(source: str) -> str:
     result = markdown2.markdown(
-        source, extras=["link-patterns"], link_patterns=LINK_PATTERNS
+        source, extras=["link-patterns"], link_patterns=_LINK_PATTERNS
     )
     return str(result)
 
 
-def to_html_Markdown(source: str) -> str:
+def _to_html_Markdown(source: str) -> str:
     result = markdown.markdown(
         # Cannot have slashes inside of blackets
         source,
@@ -28,4 +28,4 @@ def to_html_Markdown(source: str) -> str:
     return str(result)
 
 
-to_html = to_html_Markdown
+to_html = _to_html_Markdown
