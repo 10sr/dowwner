8cb05c31b9016a4e0a84f0d158e93ecf196a3c3b
10sr <8.slashes@gmail.com>
2018-12-28 17:15:01 +0900

    Fix migration script

--- dowwner/app/migrations/0002_auto_20181226_1707.py
+++ dowwner/app/migrations/0002_auto_20181226_1707.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('app', '0001_initial'),
+        ('dowwner', '0001_initial'),
     ]
 
     operations = [
