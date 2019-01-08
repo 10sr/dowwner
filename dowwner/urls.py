fd9459a6d1f9f107317d6d9cd1d855ac445a668b
10sr <8.slashes@gmail.com>
2019-01-08 15:47:47 +0900

    Refactor root urls.py

--- dowwner/urls.py
+++ dowwner/urls.py
@@ -1,31 +1,12 @@
-"""dowwner URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+"""dowwner URL Configuration"""
 from django.conf import settings
-from django.conf.urls import include, url
 from django.contrib import admin
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.urls import reverse
-
-
-def _redirect_root(request: HttpRequest) -> HttpResponse:
-    return HttpResponseRedirect("dowwner/")
+from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
-    url(r"^dowwner/admin/", admin.site.urls),
+    path("dowwner/admin/", admin.site.urls),
     # url(r"^login$", auth_views.login, name="login"),
     # Following names are imported from django.contrib.auth.urls
     # - login
@@ -35,7 +16,7 @@ urlpatterns = [
     # - password_reset_done
     # - password_reset_confirm
     # - password_reset_complete
-    url(r"^dowwenr/", include("django.contrib.auth.urls")),
-    url(r"^dowwner/", include("dowwner.app.urls")),
-    url(r"^$", _redirect_root),
+    path("dowwenr/", include("django.contrib.auth.urls")),
+    path("dowwner/", include("dowwner.app.urls")),
+    path("", RedirectView.as_view(url="dowwner/")),
 ]
