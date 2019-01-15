b53c00708504fb8ea85ee94590510fe77036b390
10sr <8.slashes@gmail.com>
2019-01-15 14:16:57 +0900

    Fix type error

--- dowwner/app/pygments.py
+++ dowwner/app/pygments.py
@@ -6,4 +6,6 @@ classname = "codehilite"
 
 def get_css(style: str) -> str:
     fmter = get_formatter_by_name(FORMAT, style=style)
-    return fmter.get_style_defs(f".{classname}")
+    css = fmter.get_style_defs(f".{classname}")
+    assert isinstance(css, str)
+    return css
