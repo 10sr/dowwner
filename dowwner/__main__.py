85a5b96065034ec095a92786782d134b14d7ede2
10sr <8.slashes@gmail.com>
2019-01-22 14:31:01 +0900

    Update __main__

--- dowwner/__main__.py
+++ dowwner/__main__.py
@@ -1,6 +1,21 @@
 import sys
+from typing import List
 
-# from dowwner import manage
+import click
+
+
+@click.group()
+def cli() -> None:
+    # print("main")
+    return
+
+
+@cli.command()
+@click.argument("name")
+def admin_user(name: str) -> None:
+    print("admiN_user {name}")
+    return
+
 
 if __name__ == "__main__":
-    print(f"TODO: Add command here {sys.argv}")
+    cli()
